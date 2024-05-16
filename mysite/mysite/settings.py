02241b1582938f206fc2fb434a5bee8f7d45a2e5
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-16 21:15:13 +0200

    /

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = "django-insecure-#esaqan)1xvmr!=a_9k3=nswl3+v1n^!cr1=4fzzo2&y!e@w*y
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,20 +69,32 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "my_service",
-            "passfile": os.getenv("PGPASSFILE"),
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "OPTIONS": {
+    #         "service": "my_service",
+    #         "passfile": os.getenv("PGPASSFILE"),
+    #     },
+    # }
+    # "default": {
+    #     "ENGINE": "mysql.connector.django",
+    #     "NAME": os.getenv("MYSQL_DATABASE"),
+    #     "USER": os.getenv("MYSQL_USER"),
+    #     "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+    #     "PORT": os.getenv("MYSQL_PORT"),
+    #     "OPTIONS": {
+    #         'auth_plugin': 'caching_sha2_password',  # not working
+    #     },
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -104,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -115,7 +124,6 @@ TIME_ZONE = "Europe/Amsterdam"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
